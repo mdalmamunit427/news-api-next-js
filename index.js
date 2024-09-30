6d07a54ccc9a73c8ec5b01d8f8aa7add0d6e57d3
mdalmamunit427 <mdalmamunit427@gmail.com>
2024-09-30 23:32:08 +0600

    cors update

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
+const cors = require('cors');
+
+// middleware
+app.use(express.json());
+app.use(cors())
 
 const data = require('./data/db.json');
 
